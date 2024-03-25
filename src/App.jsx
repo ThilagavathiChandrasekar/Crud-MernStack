56d3e4921481b28016b2025e88d5e8cfac918d75
ThilagavathiChandrasekar <cthilagavathi2003@gmail.com>
2024-03-25 18:04:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Users from './Users';
+import CreateUser from './CreateUser';
+import UpdateUser from './UpdateUser';
+
+function App() {
+  return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Users />} />
+            <Route path='/create' element={<CreateUser />} />
+            <Route path='/update/:id' element={<UpdateUser />} />
+          </Routes>
+        </BrowserRouter>
+      </div> 
+  );
+}
+
+export default App;
